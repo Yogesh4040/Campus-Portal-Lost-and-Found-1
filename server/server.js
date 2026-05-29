8b98400de7b204884a2baa388afa49b6f246c69c
Yogesh Chaudhary <yogeshsheoran4040@gmail.com>
2026-05-29 09:53:47 +0530

    Fix: adjust catch-all wildcard routing syntax for Express compatibility

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
   // Any request that doesn't match an API route redirects to index.html
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
 }
